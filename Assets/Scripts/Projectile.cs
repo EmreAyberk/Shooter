1bb42f69abae22db2dbd78ad0de9438218e12128
Emre Ayberk Kaymaz <emreayberk23@gmail.com>
2020-09-08 21:44:43 +0300

    EnemyPathfinding

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,10 +5,30 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     public float speed = 10;
+    public LayerMask collisionMask;
 
     void Update()
     {
+        float moveDistance = speed * Time.deltaTime;
+        CheckCollisions(moveDistance);
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+
+    private void CheckCollisions(float moveDistance)
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, moveDistance, collisionMask, QueryTriggerInteraction.Collide))
+        {
+            OnHitObject(hit);
+        }
+    }
+
+    void OnHitObject(RaycastHit hit)
+    {
+        print(hit.collider.gameObject.name);
+        GameObject.Destroy(gameObject);
     }
 
     public void SetSpeed(float newSpeed)
